352903e7e5f09533e779aa222dbeb074bfc062b2
quantum0059 <shivamsharma.anva@gmail.com>
2025-06-27 18:26:59 +0530

    cloudinary

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 //security config like  
 
 app.use(express.json({limit: "16kb"}))
-app.user(express.urlencoded({
+app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
 }))
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -80,6 +80,7 @@ userSchema.method.generateRefreshToken = function() {
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
